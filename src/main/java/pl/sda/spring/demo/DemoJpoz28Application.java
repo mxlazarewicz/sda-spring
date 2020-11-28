a0d4810b093d029edcb364920be83248963b9338
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-11-28 09:51:11 +0100

    init

---
+++ src/main/java/pl/sda/spring/demo/DemoJpoz28Application.java
@@ -0,0 +1,13 @@
+package pl.sda.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpoz28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpoz28Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/spring/demo/DemoJpoz28ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpoz28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
