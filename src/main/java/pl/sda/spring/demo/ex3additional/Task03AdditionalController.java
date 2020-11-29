ca06176cdb99bae58cb841fd40a4e3a2904ab525
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-11-29 14:03:50 +0100

    little refactor

--- src/main/java/pl/sda/spring/demo/ex3additional/Task03AdditionalController.java
+++ src/main/java/pl/sda/spring/demo/ex3additional/Task03AdditionalController.java
@@ -1,4 +1,4 @@
-package pl.sda.spring.demo.ex4;
+package pl.sda.spring.demo.ex3additional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,9 +9,9 @@ import pl.sda.spring.demo.ex2.Animal;
 import java.time.ZonedDateTime;
 
 @RestController
-public class Task04Controller {
+public class Task03AdditionalController {
 
-    @PostMapping("/task04")
+    @PostMapping("/task03.additional")
     public ResponseEntity<Animal> task04(@RequestBody Animal animal){
         return ResponseEntity
                 .accepted()
