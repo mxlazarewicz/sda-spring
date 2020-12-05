faf92624be4d70b0efbbc2b5b915285233c51326
Maks <mxlazarewicz@gmail.com>
2020-12-05 11:02:42 +0100

    ex5

--- src/main/java/pl/sda/spring/demo/MyFirstSpringController.java
+++ src/main/java/pl/sda/spring/demo/MyFirstSpringController.java
@@ -1,14 +1,14 @@
 package pl.sda.spring.demo;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyFirstController {
-
+public class MyFirstSprongController {
     @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name", defaultValue = "World!") String name){
+    public String hello(@RequestParam(value = "name", defaultValue = "World!")String name){
         return "Hello " + name;
+
     }
+
 }
